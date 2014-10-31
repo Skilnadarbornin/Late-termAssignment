c69cf477175441e88a9bef181abc5153e7a4cd38
peturh13 <peturh13@ru.is>
2014-10-31 18:54:23 +0000

    Made first test to check if array input is right

--- src/main/java/is/ru/tictactoe/Game.java
+++ src/main/java/is/ru/tictactoe/Game.java
@@ -8,6 +8,17 @@ public static  int doubled(int n)
         return n * 2;
 }
 
+public static char[][] ArrayInput(char x)
+{
+	char[][] tic = new char[3][3];
+     for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+                tic[i][j] = x;
+        }
+        }
+        return tic;
+}
+
 public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
