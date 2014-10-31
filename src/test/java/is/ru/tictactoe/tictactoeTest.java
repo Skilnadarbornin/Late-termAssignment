d546334150a82bb2bfbdd1ca98977fd0aa367125
Pétur Elvar Sigurðsson <peturs13@i-447fa101.greenqloud.internal>
2014-10-31 15:12:11 +0000

    modified test file and made the first test

---
+++ src/test/java/is/ru/tictactoe/tictactoeTest.java
@@ -0,0 +1,13 @@
+
+
+import static org.junit.Assert.assertEquals;
+package is.ru.tictactoe;
+
+public class tictactoeTest {
+
+	public static void main(Sting args[]){
+	org.junit.runner.JUnitCore.main("is.ru.tictactoe.tictactoeTest");
+	}
+
+}
+
