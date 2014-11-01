c2857341299b274315a7327cacb5525334ebd8fd
Einar Logi <einarh13@ru.ism>
2014-11-01 18:33:54 +0000

    changes to TicWeb.java

--- src/main/java/is/ru/tictactoe/TicWeb.java
+++ src/main/java/is/ru/tictactoe/TicWeb.java
@@ -4,7 +4,7 @@ import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 
-public class TicWebb implements SparkApplication {
+public class TicWeb implements SparkApplication {
     public static void main(String[] args){
         staticFileLocation("/public");
         SparkApplication ticweb = new TickWeb();
